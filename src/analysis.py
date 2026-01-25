b382f3ac3a7f2a7c7a3457cf8ef6a7cf8e785f85
Cornelius <60546655+loyalLogician@users.noreply.github.com>
2026-01-25 15:45:37 +0100

    added function to create GIF animation of evolution

--- src/analysis.py
+++ src/analysis.py
@@ -9,11 +9,13 @@ from scipy.ndimage import label
 from scipy.ndimage import sum
 from scipy.optimize import curve_fit
 
+from matplotlib.animation import FuncAnimation, PillowWriter
+
 
 def show_grids(grids: list, iterations=[0, 200]):
     """Plots the supplied list of grids next to each other."""
     if len(grids) > 10:  # do not allow grid lists longer than 10
-        print("The amount of grids supplied is to many, only plotting the first 10.")
+        print("The number of grids supplied is too many, only plotting the first 10.")
         grids = grids[:10]
 
     fig, axs = plt.subplots(
@@ -27,6 +29,37 @@ def show_grids(grids: list, iterations=[0, 200]):
     axs[0].set_ylabel("y")
     plt.show()
     return
+
+
+def animate_grids(grids: list, dpi=100):
+    """Create and save an animated GIF showing the temporal evolution of CA grids."""
+    # Set up the figure
+    fig, ax = plt.subplots(figsize=(8, 8))
+    im = ax.imshow(grids[0], cmap="YlGn", interpolation='nearest')
+    ax.set_title('Iteration 0', fontsize=20)
+    ax.axis('off')
+
+    # Define animation function
+    def animate(frame):
+        i, grid = frame
+        im.set_data(grid)
+        ax.set_title(f'Iteration {i}', fontsize=20)
+        return [im]
+
+    # Set up helper function to display progress
+    def progress_callback(current_frame, total_frames):
+        """Shows saving progress"""
+        if current_frame % 10 == 0:
+            print(f'Saving frame {current_frame}/{total_frames} ...')
+
+    # Create animation
+    anim = FuncAnimation(fig, animate, enumerate(grids),
+                         interval=50, save_count=len(grids))
+    print("Saving animation ... (This can take a while depending on the dpi.)")
+    filename = 'ca_simulation.gif'
+    anim.save(filename, writer=PillowWriter(fps=20), dpi=dpi,
+              progress_callback=progress_callback)
+    print(f"Saved successfully as '{filename}'")
 
 
 def powerlaw_exp(sizes, A=1e6, beta=1, s_char=1e6):
